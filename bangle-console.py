7be639d26f7326ca26951b29afb9cfe12c9d8443
Sumit Kumar Maitra <sumitkm@gmail.com>
2020-02-23 12:18:41 +0000

    Delete code addded, but never exits because of the reboot

--- bangle-console.py
+++ bangle-console.py
@@ -8,7 +8,7 @@ from bleak import BleakClient
 address = "0DB07D6F-8F0F-4CB3-A801-0D9A868BE7CE"
 UUID_NORDIC_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 UUID_NORDIC_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
-command = b"\x03\x10\
+getFileCommand = b"\x03\x10\
 var f = require('Storage').open('ftclog', 'r');\n\x10\
 var line = '';\n\x10\
 while ((line != null && line != undefined) && (line.indexOf('\xFF') == -1)){\n\x10\
@@ -16,6 +16,9 @@ while ((line != null && line != undefined) && (line.indexOf('\xFF') == -1)){\n\x
   print(line);\n\x10\
 }\n\x10\
 "
+deleteFileCommand = b"\x03\x10\var f = require('Storage').open('ftclog', 'w');\n\x10\
+ f.erase();\n\x10\
+ E.reboot();\n\x10"
 
 line = ""
 fileName = ""
@@ -40,17 +43,17 @@ async def discover_coroutine():
         devices = await discover()
         for d in devices:
             print(d)
-    
-    finally: 
+
+    finally:
         print("Done discover")
 
 print("Connecting...")
-async def command_coroutine(address, loop):
+async def getFile_coroutine(address, loop):
     async with BleakClient(address, loop=loop) as client:
         print("Connected")
         await client.start_notify(UUID_NORDIC_RX, uart_data_received)
         print("Writing command")
-        c=command
+        c=getFileCommand
         while len(c)>0:
           await client.write_gatt_char(UUID_NORDIC_TX, bytearray(c[0:20]), True)
           c = c[20:]
@@ -65,18 +68,29 @@ async def command_coroutine(address, loop):
             print("Data last recieved: " + str(dataReceived) + " Delta:" + str(lastDataReceivedDelta.seconds))
 
 
-        await client.disconnect()
-        print("\r\n\r\nDone!")
+        
         now = datetime.now()
         global fileName
-        fileName = "ftclog-" + now.strftime("%Y-%m-%d-%H-%M-%S")#-%Y-%m-%d-%H-%M-%S" 
+        fileName = "ftclog-" + now.strftime("%Y-%m-%d-%H-%M-%S")#-%Y-%m-%d-%H-%M-%S"
         f = open(fileName + ".csv", 'w')
         f.write(line)
         #print(line)
 
+async def deleteFile_coroutine(address, loop):
+    async with BleakClient(address, loop) as client:
+        print("Deleting file")
+        c=deleteFileCommand
+        while len(c)>0:
+            await client.write_gatt_char(UUID_NORDIC_TX, bytearray(c[0:20]), True)
+            c = c[20:]
+        ## Never quiet gets here
+        print("\r\n\r\nDisconnecting from Bangle !")
+        await client.disconnect()
+        print("\r\n\r\nDone!")
 try:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(discover_coroutine())
-    loop.run_until_complete(command_coroutine(address, loop))
+    loop.run_until_complete(getFile_coroutine(address, loop))
+    loop.run_until_complete(deleteFile_coroutine(address, loop))
 finally:
     print("finally!")
